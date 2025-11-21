eaffb27a074b03a922596192c60bdebc1f3daf85
Abdulla Vennakode <abdullacp9048@gmail.com>
2025-11-21 16:28:45 +0530

    created invetment model

---
+++ model/investment.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const investmentSchema = mongoose.Schema({
+    investorId:{
+        type:Schema.Types.ObjectId,
+        ref:"user",
+        required:[true,"Investor Id is Required"]
+    },
+    opportunityId:{
+        type:Schema.Types.ObjectId,
+        ref:"investmentOpp",
+        required:[true,"Investment Id is Required"]
+    },
+    amount:{
+        type:Number,
+        required:[true,'Amout is Required']
+    },
+    sharesOrUnits:{
+        type:Number,
+        required:[true,"Shares or Units are Required"]
+    },
+    transactionDate:{
+        type:Date
+    },
+    status:{
+        type:String,
+        enum:["Pending","Accepted","Rejected"],
+        default:"Pending"
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+});
+
+export const investmentModel = mongoose.model("investment",investmentSchema);
